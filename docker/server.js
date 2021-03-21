33a7def56664e0fd607080e04ee6f27ecf065ebd
Michael Nelson <michaelnelson@Michaels-iMac.local>
2021-03-21 13:46:59 -0700

    Added default values to form inputs

--- docker/server.js
+++ docker/server.js
@@ -17,10 +17,12 @@ function validate(urlQuery) {
 }
 
 function callOpenMaps(urlQuery) {
+  // TODO change input to omit "+" to avoid conversion
   // convert latitude value
   // example: " 48.4251378"
   const lat = '+' + urlQuery["lat"].trim()
 
+  // TODO use a template
   const fullPath = "http://openmaps.gov.bc.ca/geo/pub/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=pub%3AWHSE_ADMIN_BOUNDARIES.BCHA_CMNTY_HEALTH_SERV_AREA_SP&srsname=EPSG%3A4326&cql_filter=INTERSECTS(SHAPE%2CSRID%3D4326%3BPOINT(" + urlQuery["long"] + lat + "))&propertyName=CMNTY_HLTH_SERV_AREA_CODE%2CCMNTY_HLTH_SERV_AREA_NAME&outputFormat=application%2Fjson"
 
   const response = syncrequest('GET', fullPath)
