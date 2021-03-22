a37acbf94fb905e37beba5f7eefd2b63e7ad90aa
Michael Nelson <michaelnelson@Michaels-iMac.local>
2021-03-21 17:58:44 -0700

    Added Apache 2.0 license; small improvements

--- docker/server.js
+++ docker/server.js
@@ -2,31 +2,23 @@
 const url = require('url')
 const syncrequest = require('sync-request')
 const ronin     = require( 'ronin-server' )
-const mocks     = require( 'ronin-mocks' )
-const database  = require( 'ronin-database' )
 const mysql = require('mysql')
 
-function validate(urlQuery) {
+function validate(value, name) {
   // TODO validate data - regexp?
-  // TODO break validation out to function taking a parameter
-  if (!urlQuery["lat"]) {
-    return "lat is missing"
-  }
-  if (!urlQuery["long"]) {
-    return "long is missing"
+  if (!value) {
+    return `No value was found for '${name}'. Please ensure a value was entered.`
   }
 }
 
 function callOpenMaps(urlQuery) {
-  // TODO change input to omit "+" to avoid conversion
   // convert latitude value
-  // example: " 48.4251378"
+  // example: " 48.4251378" (if '+' is sent in POST to this server)
+  // example: "48.4251378"
   const lat = '+' + urlQuery["lat"].trim()
 
-  // TODO use a template
-  const fullPath = "http://openmaps.gov.bc.ca/geo/pub/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=pub%3AWHSE_ADMIN_BOUNDARIES.BCHA_CMNTY_HEALTH_SERV_AREA_SP&srsname=EPSG%3A4326&cql_filter=INTERSECTS(SHAPE%2CSRID%3D4326%3BPOINT(" + urlQuery["long"] + lat + "))&propertyName=CMNTY_HLTH_SERV_AREA_CODE%2CCMNTY_HLTH_SERV_AREA_NAME&outputFormat=application%2Fjson"
-
-  const response = syncrequest('GET', fullPath)
+  const apiUrl = `http://openmaps.gov.bc.ca/geo/pub/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=pub%3AWHSE_ADMIN_BOUNDARIES.BCHA_CMNTY_HEALTH_SERV_AREA_SP&srsname=EPSG%3A4326&cql_filter=INTERSECTS(SHAPE%2CSRID%3D4326%3BPOINT(${urlQuery['long']}${lat}))&propertyName=CMNTY_HLTH_SERV_AREA_CODE%2CCMNTY_HLTH_SERV_AREA_NAME&outputFormat=application%2Fjson`
+  const response = syncrequest('GET', apiUrl)
   const body = response.getBody('utf-8')
   return JSON.parse(body).features[0].properties['CMNTY_HLTH_SERV_AREA_NAME']
 }
@@ -45,9 +37,6 @@ function callOpenMaps(urlQuery) {
 //     "  </body>" +
 //     "</html>"
 // }
-
-// database.connect( process.env.CONNECTIONSTRING )
-
 // const hostname = '127.0.0.1';
 // const port = 8000;
 //
@@ -90,12 +79,15 @@ const server = ronin.server()
 server.use( '/bcapp', (req, res) => {
   incrementApiCallNumInDatabase()
   const urlQuery = url.parse(req.url, true).query
-  let errorMessage = validate(urlQuery)
-  if (errorMessage) {
-    return res.json({"error":errorMessage})
+  const latErrorMessage = validate(urlQuery["lat"], "latitude")
+  if (latErrorMessage) {
+    return res.json({"error":latErrorMessage})
+  }
+  const longErrorMessage = validate(urlQuery["long"], "longitude")
+  if (longErrorMessage) {
+    return res.json({"error":longErrorMessage})
   }
   const result = callOpenMaps(urlQuery)
   return res.json({"CMNTY_HLTH_SERV_AREA_NAME": result})
 })
-server.use( '/', mocks.server( server.Router(), false, true ) )
 server.start()
